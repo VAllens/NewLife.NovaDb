90eae30af8ae9853f014dc39898325a9290dea9f
智能大石头 <nnhy@vip.qq.com>
2026-02-18 10:04:20 +0800

    .NET 8 原生，嵌入式/服务器双模，融合关系型、时序、消息队列能力的混合数据库。NewLife 生态核心基础设施，替代 SQLite/MySQL/Redis/TDengine 在特定场景的使用。

---
+++ README.md
@@ -0,0 +1,4 @@
+# NewLife.NovaDb
+
+.NET 8 原生，嵌入式/服务器双模，融合关系型、时序、消息队列能力的混合数据库。
+NewLife 生态核心基础设施，替代 SQLite/MySQL/Redis/TDengine 在特定场景的使用。
---
+++ Test/Program.cs
@@ -0,0 +1,27 @@
+﻿using NewLife.Log;
+
+namespace Test;
+
+class Program
+{
+    static void Main(String[] args)
+    {
+        XTrace.UseConsole();
+
+        try
+        {
+            Test1();
+        }
+        catch (Exception ex)
+        {
+            XTrace.WriteException(ex);
+        }
+
+        Console.WriteLine("OK!");
+        Console.ReadKey();
+    }
+
+    static async void Test1()
+    {
+    }
+}
